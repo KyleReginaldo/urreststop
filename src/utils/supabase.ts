609faf3bb4c6198e83286b56b93454c4ad0aa586
kyle reginaldo <97393306+KyleReginaldo@users.noreply.github.com>
2025-04-05 09:53:04 +0800

    disploy

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -3,4 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl!, supabaseKey!);
+export const supabase = createClient(
+  supabaseUrl ?? "https://fpdlruaykocfvyeghnas.supabase.co",
+  supabaseKey ??
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwZGxydWF5a29jZnZ5ZWdobmFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2MDMzMzIsImV4cCI6MjA1OTE3OTMzMn0.QEDZfNbrC_iioVM1SWBHGn6hxdXxL1AOPD02hcKWTiE"
+);
