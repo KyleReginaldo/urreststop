a09fc94ff23cf62c92e678429e944dd0a6e0f788
kyle reginaldo <97393306+KyleReginaldo@users.noreply.github.com>
2025-04-12 12:09:26 +0800

    da

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { GlobalContextProvider } from "./context/store";
 import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +26,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <GlobalContextProvider> {children}</GlobalContextProvider>
+        {children}
       </body>
     </html>
   );
